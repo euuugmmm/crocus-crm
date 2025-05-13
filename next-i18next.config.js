406b3e22f31081922916ae25fa4dea7fac885186
euuugmmm <euuugmmm@gmail.com>
2025-05-13 03:05:37 +0300

    multi_lang

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,11 @@
 // next-i18next.config.js
+const path = require("path");
+
 module.exports = {
   i18n: {
-    defaultLocale: 'ua',
-    locales: ['en', 'ru', 'ua'],
+    defaultLocale: "ua",
+    locales: ["en", "ru", "ua"],
+ 
   },
   localePath: path.resolve("./public/locales"),
 };
