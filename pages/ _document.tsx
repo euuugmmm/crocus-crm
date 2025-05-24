1c15c2f945a5f24493d650d030e939e8422b4577
euuugmmm <euuugmmm@gmail.com>
2025-05-24 10:03:13 +0300

    Яндекс Метрика

--- pages/ _document.tsx
+++ pages/ _document.tsx
@@ -17,36 +17,36 @@ class MyDocument extends Document {
     return (
       <Html lang="ru">
         <Head>
-          {/* Yandex.Metrika */}
+          {/* Yandex.Metrika: асинхронный загрузчик */}
+          <script
+            async
+            src="https://mc.yandex.ru/metrika/tag.js"
+          />
+          {/* Инициализация счётчика */}
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();
-                for (var j = 0; j < document.scripts.length; j++) {
-                  if (document.scripts[j].src === r) { return; }
-                }
-                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,
-                a.parentNode.insertBefore(k,a)
-                })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
+                window.ym = window.ym || function() {
+                  (window.ym.a = window.ym.a || []).push(arguments);
+                };
                 ym(102132289, "init", {
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
                 });
               `,
             }}
           />
         </Head>
         <body>
+          {/* Для случаев без JS */}
           <noscript>
             <div>
               <img
                 src="https://mc.yandex.ru/watch/102132289"
                 style={{ position: "absolute", left: "-9999px" }}
-                alt=""
+                alt="yandex metrika"
               />
             </div>
           </noscript>
