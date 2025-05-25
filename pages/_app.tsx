6bd5a33bc47a2c2aeeb85665a7e7eeaaee8d4ce3
euuugmmm <euuugmmm@gmail.com>
2025-05-25 11:12:39 +0300

    Яндекс Метрика

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,55 +1,41 @@
 // pages/_app.tsx
-import type { AppProps } from 'next/app';
-import Script from 'next/script';
-import { appWithTranslation } from 'next-i18next';
-import nextI18NextConfig from '../next-i18next.config.js';
-import { AuthProvider } from '../context/AuthContext';
-import '../styles/globals.css';
+import type { AppProps } from "next/app";
+import { appWithTranslation } from "next-i18next";
+import nextI18NextConfig from "../next-i18next.config.js";
+import { AuthProvider } from "../context/AuthContext";
+import "../styles/globals.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      {/* 1. Подключаем сам скрипт Яндекс.Метрики */}
+    <AuthProvider>
+      {/*
+        Подключаем tag.js Яндекс.Метрики сразу после того,
+        как Next.js перешёл в клиентский режим
+      */}
       <Script
-        strategy="afterInteractive"
         src="https://mc.yandex.ru/metrika/tag.js"
-      />
-
-      {/* 2. Инициализируем счётчик */}
-      <Script
-        id="yandex-metrika-init"
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.ym = window.ym || function(){(window.ym.a = window.ym.a || []).push(arguments)};
-            ym(102132289, 'init', {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true,
-              webvisor:true
-            });
-          `,
-        }}
       />
+      {/*
+        Инициализируем счётчик — тоже послеInteractive,
+        чтобы DOM уже был доступен
+      */}
+      <Script id="yandex-init" strategy="afterInteractive">
+        {`
+          ym(102132289, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+          });
+        `}
+      </Script>
 
-      {/* 3. Для пользователей без JavaScript */}
-      <noscript>
-        <div>
-          <img
-            src="https://mc.yandex.ru/watch/102132289"
-            style={{ position: 'absolute', left: '-9999px' }}
-            alt="Yandex.Metrika"
-          />
-        </div>
-      </noscript>
-
-      {/* 4. Остальная логика приложения */}
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </>
+      <Component {...pageProps} />
+    </AuthProvider>
   );
 }
 
-// Передаём конфиг next-i18next вторым аргументом
+// Оборачиваем в next-i18next
 export default appWithTranslation(MyApp, nextI18NextConfig);
